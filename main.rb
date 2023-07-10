fc8662b15d3f18ba47add2b77b0caec201ee5374
OsmanFP <osmanwako411@gmail.com>
2023-07-10 16:31:17 +0300

    Fix linter ruboco error

--- main.rb
+++ main.rb
@@ -4,14 +4,25 @@ end
 
 def startapp(message)
   puts message
+
   loop do
     puts "\nPlease choose an options by entering a number"
-    puts '1 - Exit'
+    puts "\n1 - List all books"
+    puts "2 - List all music albums"
+    puts "3 - List of games"
+    puts "4 - List all genres"
+    puts "5 - List all labels"
+    puts "6 - List all authors"
+    puts "8 - Add a book"
+    puts "9 - Add a music album"
+    puts "10 - Add a game"
+    puts "11 - Exit"
+
     order = gets.chomp
-    break if order == '1'
+    break if order == "11"
 
     youroption(order)
   end
 end
 
-startapp('We come to Catelog of my things')
+startapp("We come to Catelog of my things")
---
+++ spec/item_spec.rb
@@ -0,0 +1,10 @@
+describe "Test of Item Class" do
+  before(:context) do
+  end
+  context "Test Methods and accessor of items" do
+    it "Test move_to_archived method" do
+      value = true
+      expect(value).to be true
+    end
+  end
+end
