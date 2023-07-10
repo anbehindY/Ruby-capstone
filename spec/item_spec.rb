06c3b5fbb437a50819d599db0ed20dfcafb65f39
anbehindY <yanminthwin@gmail.com>
2023-07-10 20:13:07 +0630

    Modify test for item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,7 +2,7 @@ require_relative '../classes/item'
 
 describe 'Test of Item Class' do
   before(:context) do
-    @item = Item.new('2023-07-09', false)
+    @item = Item.new('2023-07-09')
   end
   context 'Test Methods and accessor of items' do
     it 'Test move_to_archived method' do
