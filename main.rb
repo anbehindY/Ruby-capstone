b2ba0ce23949bf96e66572b96d267830c8892782
OsmanFP <osmanwako411@gmail.com>
2023-07-10 16:10:49 +0300

    Add main start app

---
+++ main.rb
@@ -0,0 +1,16 @@
+def youroption(option)
+  puts option
+end
+
+def startapp(message)
+  puts message
+  loop do
+    puts "\nPlease choose an options by entering a number"
+    puts "1 - Exit"
+    order = gets.chomp
+    break if order == "1"
+    youroption(order)
+  end
+end
+
+startapp("We come to Catelog of my things")
