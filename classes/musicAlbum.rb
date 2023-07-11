fe81b66a4403705cdad240bc3eb93c7479d0dd3d
Daniel-Kigozi <kdanalex69@gmail.com>
2023-07-11 12:13:37 +0300

    add music and genre class

---
+++ classes/musicAlbum.rb
@@ -0,0 +1,25 @@
+require './item'
+
+# MusicAlbum class.
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  @music_albums = []
+  def initialize(on_spotify:, **attributes)
+    super(attributes)
+    @on_spotify = on_spotify
+    self.class.all << self
+  end
+
+  def can_be_archived?
+    super && @on_spotify == true
+  end
+
+  def self.all
+    @music_albums
+  end
+
+  def self.all=(albums)
+    @music_albums = albums
+  end
+end
