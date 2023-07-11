f51e8117eb1be30bf7f439389514d4f08f866d1d
OsmanFP <osmanwako411@gmail.com>
2023-07-11 17:12:00 +0300

    Update User input structure

--- main.rb
+++ main.rb
@@ -1,5 +1,30 @@
+require_relative("app")
+require_relative("userinput")
+
 def youroption(option)
-  puts option
+  case option
+  when "1"
+    list_books
+  when "2"
+    list_musicalbums
+  when "3"
+    list_games
+  when "4"
+    list_genres
+  when "5"
+    list_labels
+  when "6"
+    list_author
+  when "7"
+    add_book
+  when "8"
+    add_musicalbum
+  when "9"
+    add_game
+  when "10"
+    puts "Thanks for using our Application!"
+    exit
+  end
 end
 
 def startapp(message)
@@ -8,21 +33,18 @@ def startapp(message)
   loop do
     puts "\nPlease choose an options by entering a number"
     puts "\n1 - List all books"
-    puts '2 - List all music albums'
-    puts '3 - List of games'
-    puts '4 - List all genres'
-    puts '5 - List all labels'
-    puts '6 - List all authors'
-    puts '8 - Add a book'
-    puts '9 - Add a music album'
-    puts '10 - Add a game'
-    puts '11 - Exit'
-
+    puts "2 - List all music albums"
+    puts "3 - List of games"
+    puts "4 - List all genres"
+    puts "5 - List all labels"
+    puts "6 - List all authors"
+    puts "7 - Add a book"
+    puts "8 - Add a music album"
+    puts "9 - Add a game"
+    puts "10 - Exit"
     order = gets.chomp
-    break if order == '11'
-
     youroption(order)
   end
 end
 
-startapp('We come to Catelog of my things')
+startapp("We come to Catelog of my things")
