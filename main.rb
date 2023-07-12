79eecacb6f9b5bdf8782f9bbfa024ac36d9bf13d
OsmanFP <osmanwako411@gmail.com>
2023-07-12 18:58:01 +0300

    Update your input option

--- main.rb
+++ main.rb
@@ -1,43 +1,53 @@
-require_relative 'app'
+require_relative "app"
 
-def list_options
-  puts "\nPlease choose an option by entering a number"
-  puts "\n1 - List all books"
-  puts '2 - List all music albums'
-  puts '3 - List of games'
-  puts '4 - List all genres'
-  puts '5 - List all labels'
-  puts '6 - List all authors'
-  puts '8 - Add a book'
-  puts '9 - Add a music album'
-  puts '10 - Add a game'
-  puts '11 - Exit'
-end
-
-def your_option(app, option)
+def your_option(option)
   case option
-  when '1'
-    app.list_books
-  when '5'
-    app.list_labels
-  when '8'
-    app.add_book
+  when "1"
+    APP.list_books
+  when "2"
+    puts "list_musicalbums"
+  when "3"
+    puts "list_games"
+  when "4"
+    puts "list_genres"
+  when "5"
+    puts "list_labels"
+  when "6"
+    puts "list_author"
+  when "7"
+    APP.add_book
+  when "8"
+    puts "add_musicalbum"
+  when "9"
+    puts "add_game"
+  when "10"
+    puts "Thanks for using our Application!"
+    exit
   end
 end
 
 def start_app(message)
-  app = App.new
   puts message
-
   loop do
-    list_options
+    puts "\nPlease choose an options by entering a number"
+    puts "\n1 - List all books"
+    puts "2 - List all music albums"
+    puts "3 - List of games"
+    puts "4 - List all genres"
+    puts "5 - List all labels"
+    puts "6 - List all authors"
+    puts "7 - Add a book"
+    puts "8 - Add a music album"
+    puts "9 - Add a game"
+    puts "10 - Exit"
     order = gets.chomp
-    if order == '11'
-      app.save_data
-      break
+    if ("1".."10").include? order
+      your_option(order)
+    else
+      puts "\nInvalid input. please try again!"
     end
-    your_option(app, order)
   end
 end
 
-start_app('Welcome to Catalog of my things')
+APP = App.new
+start_app("Welcome to Catalog of my things")
